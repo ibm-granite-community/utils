5e467643b8d0f0fd09075d1a18afa8f0918a8e4b
Adam Pingel <pingel@gmail.com>
2024-09-13 13:26:47 -0400

    call set_api_key from find_langchain_model
    Signed-off-by: Adam Pingel <pingel@gmail.com>

--- ibm_granite_community/langchain_utils.py
+++ ibm_granite_community/langchain_utils.py
@@ -1,8 +1,9 @@
-
+from ibm_granite_community.notebook_utils import set_api_key
 
 def find_langchain_model(platform, model_id, **model_kwargs):
     if platform.lower() == "replicate":
         from langchain_community.llms import Replicate
+        set_api_key()
         model = Replicate(
             model=model_id,
             model_kwargs=model_kwargs
