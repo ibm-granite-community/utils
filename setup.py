66de8f9a04efca4c5e33f5f5eb54efdd3b5f2776
Fayvor Love <fayvor@gmail.com>
2024-10-03 12:41:30 -0400

    Delete recipes; pare dependencies; add notebook utils tests
    Signed-off-by: Fayvor Love <fayvor@gmail.com>

--- setup.py
+++ setup.py
@@ -7,16 +7,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'python-dotenv',
-        'langchain_community<0.3.0',
-        "langchain_ollama<0.2.0",
-        "replicate",
-        "langchain-pinecone",
-        "langchain-milvus",
-        "langchain-chroma",
-        "langchain_ibm",
-        "langchain_huggingface",
-        "requests==2.32.3", # This is a `google-colab 1.0.0` dependency
+        dotenv,
     ],
     author='The IBM Granite Community Team',
     author_email='trevor.d.grant@gmail.com',
